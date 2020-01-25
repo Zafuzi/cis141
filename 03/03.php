e6656f264d4fbbf8796675b4d64377cf695a1a2f
Your Name <you@example.com>
2020-01-25 15:32:30 -0800

    started unit 3, exercise done, word docs not done

---
+++ 03/03.php
@@ -0,0 +1,27 @@
+<?php 
+	$loan_amt = 0;
+	$deposit  = 0;
+	$msg = "";
+
+	printf("\nEnter the loan amount: ");
+	fscanf(STDIN, "%f", $loan_amt);
+
+	if($loan_amt < 25000) {
+		$msg = "5% of loan value";
+		$deposit = $loan_amt * 0.05;
+	} else if ( $loan_amt < 50000 ) {
+		$msg = "$1250 + 10% of loan over $25,000";
+		$deposit = 1250 + ($loan_amt - 25000) * 0.10;
+	} else if ( $loan_amt <= 100000 ) {
+		$msg = "$5000 + 10% of loan over $50,000";
+		$deposit = 5000 + ($loan_amt - 50000) * 0.10;
+	} else {
+		// Loans over 100,000 are not allowed
+		$msg = "LOANS OVER $100,000 ARE NOT ALLOWED!";
+		$deposit = 0; 
+	}
+
+	printf("\n\t   Loan Amount : $%s", number_format($loan_amt, 2, '.', ','));
+	printf("\n\t  Deposit Rule : %s", $msg);
+	printf("\n\t Total Deposit : $%s\n\n", number_format($deposit, 2, '.', ','));
+?>
